d0dfab42beac4e76319196dc9f485aa715d518d0
revster29 <revsterregacho@gmail.com>
2026-08-08 15:26:26 +0800

    Create largestNumber.java

---
+++ Object-Oriented Programming/largestNumber.java
@@ -0,0 +1,17 @@
+public class largestNumber{
+    public static void main (String [] args){
+        int a=29, b=67, c=19;
+        int largest;
+
+        if (a>=b && a>=c){
+            largest=a;
+        }
+        else if (b>=a && b>=c){
+            largest=b;
+        }
+        else{
+            largest=c;
+        }
+        System.out.println("The largest number is: "+ largest);
+    }
+}
