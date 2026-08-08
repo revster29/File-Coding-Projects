148b0f02edf24ec214e06fc3c302f5f924617b19
revster29 <revsterregacho@gmail.com>
2026-08-08 15:25:05 +0800

    Create totalCalculator.java

---
+++ Object-Oriented Programming/totalCalculator.java
@@ -0,0 +1,11 @@
+public class totalCalculator{
+    public static void main (String [] args){
+
+        int x=80;
+        int y=21;
+
+        int total=x+y;
+
+        System.out.println("Total: " + total);
+    }
+}
