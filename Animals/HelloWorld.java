13098cd9a2cec04857845b1dfc83201d4ccedfce
revster29 <revsterregacho@gmail.com>
2026-08-09 16:40:36 +0800

    Commit HelloWorld.java

---
+++ Animals/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        dog animal1 = new dog();
+        cat animal2 = new cat();
+        duck animal3 = new duck();
+        animal1.sayHello();
+        animal2.sayHello();
+        animal3.sayHello();
+    }
+}
